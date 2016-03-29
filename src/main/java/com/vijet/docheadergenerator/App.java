41d8bdf6e6e714182fdf11bc6c2c0c73794cff32
VIJET BADIGANNAVAR <bvijet@gmail.com>
2016-03-29 13:25:51 -0400

    Setup the maven to create a runnable jar

---
+++ src/main/java/com/vijet/docheadergenerator/App.java
@@ -0,0 +1,14 @@
+package com.vijet.docheadergenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(args[0]);
+    }
+}
