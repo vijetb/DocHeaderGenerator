05f124c7c5f05dc8e9c250001ab731fd31a476ad
VIJET BADIGANNAVAR <bvijet@gmail.com>
2016-03-30 21:24:44 -0400

    Documentation for the StartUp class

--- src/main/java/com/vijet/docheadergenerator/DocHeaderGeneratorStartUp.java
+++ src/main/java/com/vijet/docheadergenerator/DocHeaderGeneratorStartUp.java
@@ -7,9 +7,23 @@ import com.vijet.docheadergenerator.header.HeaderText;
 import com.vijet.docheadergenerator.headerappender.HeaderFileAppender;
 import com.vijet.docheadergenerator.util.Constants;
 import com.vijet.docheadergenerator.util.FolderUtil;
-
+/**
+ * Main class that is initiated by the Library. 
+ * It checks for valid number of arguments. The general format for executing the library
+ * is:
+ * java -jar docHeaderGenerator.jar <path_to_project_root> <path_to_file> <file_format> [file_format]
+ * @author Vijet Badigannavar
+ * @Date 03-30-2016
+ */
 public class DocHeaderGeneratorStartUp {
-
+	/**
+	 * Entry point for the library. Checks for the minimum number of arguments.
+	 * Checks if the project_root is a folder and setup the headerText.
+	 * It also setup all the file formats that needs to be modified and iterates
+	 * through each of the files in the project_root and modifies it accordingly.
+	 * @param args
+	 * @throws IOException if all the arguments are not specified.
+	 */
 	public static void main(String[] args) throws IOException {
 		if(args.length < 3){
 			throw new IllegalArgumentException(Constants.INVALID_ARGUMENTS);
