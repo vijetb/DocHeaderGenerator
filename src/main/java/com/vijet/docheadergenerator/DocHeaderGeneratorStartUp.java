f8cf36a0428ba9aa166d86828f066290c84abe2f
VIJET BADIGANNAVAR <bvijet@gmail.com>
2016-03-30 21:18:18 -0400

    Removed the *.* condition

--- src/main/java/com/vijet/docheadergenerator/DocHeaderGeneratorStartUp.java
+++ src/main/java/com/vijet/docheadergenerator/DocHeaderGeneratorStartUp.java
@@ -11,24 +11,17 @@ import com.vijet.docheadergenerator.util.FolderUtil;
 public class DocHeaderGeneratorStartUp {
 
 	public static void main(String[] args) throws IOException {
-		/*path: java -jar docheaderGenerator HelloWorld header.txt *.* */
-		if(args.length < 1){
+		if(args.length < 3){
 			throw new IllegalArgumentException(Constants.INVALID_ARGUMENTS);
-		}
-		//TODO:
-		// copy the whole folder
-		
+		}		
 		//Check if the input is a folder.
 		FolderUtil.checkForFolder(args[0]);
-		
 		//setup the header
 		HeaderText headerText = new HeaderText(args[1]);
-		System.out.println(headerText.getHeaderText()); // will be removed
 		//setup the formats
 		AcceptedFileFormats fileFormats = new AcceptedFileFormats(args);
 		// Iterate through the source folder and add the header
 		new HeaderFileAppender(args[0],headerText,fileFormats);
-		
 		System.out.println("DocHeaderGenerator finished updating the files..!!");
 	}
 
